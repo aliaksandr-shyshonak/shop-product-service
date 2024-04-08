ac60a5e322d2c7261d16022569630bedc3bc6d7d
aliaksandr_shyshonak <aliaksandr_shyshonak@epam.com>
2024-04-08 18:08:41 +0400

    feat: configure app config service

--- src/functions/HttpExample.ts
+++ src/functions/HttpExample.ts
@@ -1,15 +1,42 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { AppConfigurationClient } from "@azure/app-configuration";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
 
-export async function HttpExample(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+const CONNECTION_STRING = process.env.AZURE_APP_CONFIG_CONNECTION_STRING;
 
-    const name = request.query.get('name') || await request.text() || 'world';
+export async function HttpExample(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+  let configValue;
 
-    return { body: `Hello, ${name}!` };
-};
+  try {
+    const client = new AppConfigurationClient(CONNECTION_STRING);
+    const configs = await client.getConfigurationSetting({
+      key: "NAME_FROM_APP_CONFIG",
+    });
+    configValue = configs.value;
+  } catch (e) {
+    context.log("Fail to get config");
+  }
 
-app.http('HttpExample', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: HttpExample
+  const name =
+    request.query.get("name") ||
+    (await request.text()) ||
+    configValue ||
+    "world";
+
+  return { body: `Hello, ${name}!` };
+}
+
+app.http("HttpExample", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: HttpExample,
+  route: "example",
 });
