b21b7bd7cffb92fcf8a7b5dc554cef0e4464a400
aliaksandr_shyshonak <aliaksandr_shyshonak@epam.com>
2024-04-24 19:04:06 +0400

    feat: add rbac

--- src/common/db-client-factory.ts
+++ src/common/db-client-factory.ts
@@ -1,13 +1,14 @@
 import { CosmosClient } from "@azure/cosmos";
+import { DefaultAzureCredential } from "@azure/identity";
 
 export class DbClientFactory {
   private static instance: CosmosClient;
 
   public static getInstance(): CosmosClient {
     if (!DbClientFactory.instance) {
-      const key = process.env.COSMOS_KEY;
       const endpoint = process.env.COSMOS_ENDPOINT;
-      DbClientFactory.instance = new CosmosClient({ endpoint, key });
+      const aadCredentials = new DefaultAzureCredential();
+      DbClientFactory.instance = new CosmosClient({ endpoint, aadCredentials });
     }
     return DbClientFactory.instance;
   }
