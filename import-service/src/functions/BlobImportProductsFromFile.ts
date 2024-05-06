7866dad80d02aba810eb3ac092ef48d9798c988a
aliaksandr_shyshonak <aliaksandr_shyshonak@epam.com>
2024-05-06 20:50:24 +0400

    feat(import-service) implement file upload and parse

--- import-service/src/functions/BlobImportProductsFromFile.ts
+++ import-service/src/functions/BlobImportProductsFromFile.ts
@@ -1,4 +1,11 @@
 import { app, InvocationContext } from "@azure/functions";
+import {
+  BlobDeleteResponse,
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+} from "@azure/storage-blob";
+import { parse } from "csv-parse";
+import { generateSasToken } from "../common/token";
 
 export async function BlobImportProductsFromFile(
   blob: Buffer,
@@ -7,10 +14,71 @@ export async function BlobImportProductsFromFile(
   context.log(
     `Storage blob function processed blob "${context.triggerMetadata.name}" with size ${blob.length} bytes`
   );
+  try {
+    const parsedData = new Promise((resolve, reject) => {
+      const importedItems = [];
+      parse(blob, {
+        columns: true,
+      })
+        .on("data", (data) => importedItems.push(data))
+        .on("error", (e) => {
+          context.log("Failed to parse file - ", e.message);
+          reject(e);
+        })
+        .on("end", () => {
+          resolve(importedItems);
+        });
+    });
+    const result = await parsedData;
+    if (Array.isArray(result)) {
+      for (const item of result) {
+        JSON.stringify(item);
+        context.log(item);
+      }
+    } else {
+      context.log("No items found.");
+    }
+  } catch (e) {
+    context.log("Fail to parse blob - ", e.message);
+  }
+  await copyToParsedFolder(context);
+}
+
+async function copyToParsedFolder(context: InvocationContext): Promise<void> {
+  const file = String(context.triggerMetadata.name);
+  try {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.CONNECTION_IMPORT_FILES_STORAGE_ACCOUNT
+    );
+    const uploadedContainerClient =
+      blobServiceClient.getContainerClient("uploaded");
+    const sourceBlob = uploadedContainerClient.getBlobClient(file);
+    const parsedContainerClient =
+      blobServiceClient.getContainerClient("parsed");
+    const targetBlob = parsedContainerClient.getBlobClient(file);
+    const sasToken = generateSasToken(
+      sourceBlob.containerName,
+      sourceBlob.name,
+      sourceBlob.credential as StorageSharedKeyCredential
+    ).toString();
+    const sourceBlobSASURL: string = sourceBlob.url + "?" + sasToken;
+
+    const copyPoller = await targetBlob.beginCopyFromURL(sourceBlobSASURL);
+    await copyPoller.pollUntilDone();
+    context.log(`${file} copied to parsed folder.`);
+    const blobDeleteResponse: BlobDeleteResponse = await sourceBlob.delete({
+      deleteSnapshots: "include",
+    });
+    if (!blobDeleteResponse.errorCode) {
+      context.log(`${file} was deleted from uploaded folder.`);
+    }
+  } catch (e) {
+    context.log("Failed to move file", e.message);
+  }
 }
 
 app.storageBlob("BlobImportProductsFromFile", {
   path: "uploaded/{name}",
-  connection: "",
+  connection: "CONNECTION_IMPORT_FILES_STORAGE_ACCOUNT",
   handler: BlobImportProductsFromFile,
 });
--- import-service/src/functions/HttpGetImportProductsFiles.ts
+++ import-service/src/functions/HttpGetImportProductsFiles.ts
@@ -4,6 +4,16 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
+import {
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+} from "@azure/storage-blob";
+import { generateSasToken } from "../common/token";
+
+const HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "text/plain",
+};
 
 export async function HttpGetImportProductsFiles(
   request: HttpRequest,
@@ -11,13 +21,40 @@ export async function HttpGetImportProductsFiles(
 ): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
-  const name = request.query.get("name") || (await request.text()) || "world";
+  const name = request.query.get("name");
+  context.log(`Passed file name: "${request.url}"`);
 
-  return { body: `Hello, ${name}!` };
+  if (!name) {
+    return {
+      body: "Bad request. Please pass a file name on the query string",
+      status: 400,
+      headers: HEADERS,
+    };
+  }
+
+  const blobServiceClient = BlobServiceClient.fromConnectionString(
+    process.env.CONNECTION_IMPORT_FILES_STORAGE_ACCOUNT
+  );
+  const containerClient = blobServiceClient.getContainerClient("uploaded");
+  const blobClient = containerClient.getBlobClient(name);
+  context.log(`Prepared blob url "${blobClient.url}"`);
+
+  const sasToken = generateSasToken(
+    containerClient.containerName,
+    name,
+    blobServiceClient.credential as StorageSharedKeyCredential
+  ).toString();
+
+  return {
+    body: blobClient.url + "?" + sasToken,
+    status: 200,
+    headers: HEADERS,
+  };
 }
 
 app.http("HttpGetImportProductsFiles", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: HttpGetImportProductsFiles,
+  route: "import",
 });
